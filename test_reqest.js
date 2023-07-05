8868b36321e41a65d51eb63f32c7d1c03b59bf18
Артем <artyom27000@mail.ru>
2023-07-06 01:30:53 +0300

    ошибки

--- test_reqest.js
+++ test_reqest.js
@@ -7,12 +7,13 @@ const clc = require('cli-color');
 const {pathfinder, Movements, goals: {GoalNear, GoalBlock, GoalFollow}} = require('mineflayer-pathfinder');
 const password = '0303708000'
 const bot = mineflayer.createBot({
-        host: process.argv[2],
-        // port: process.argv[3] ? process.argv[3] : NaN,
-        username: process.argv[4] ? process.argv[4] : 'C',
-        password: process.argv[5] ? process.argv[5] : password,
-    })
+    host: process.argv[2],
+    // port: process.argv[3] ? process.argv[3] : NaN,
+    username: process.argv[4] ? process.argv[4] : 'C',
+    password: process.argv[5] ? process.argv[5] : password,
+})
 app.use(express.static(__dirname))
+
 // --------------- Ф У Н К Ц И И  -----------------//
 function login() {
     bot.chat(`/register ${process.argv[5]} ${process.argv[5]}  `)
@@ -22,6 +23,7 @@ function login() {
     const defaultMove = new Movements(bot, mcData)
     bot.pathfinder.setMovements(defaultMove)
 }
+
 function come(target) {
     if (!target) {
         bot.chat(`/msg ${target.displayName} я тебя не вижу`)
@@ -29,10 +31,12 @@ function come(target) {
     }
     bot.pathfinder.setGoal(new GoalFollow(target, 1), true)
 }
+
 function stop(target) {
     bot.pathfinder.setGoal(null, 1)
     bot.chat(`/msg ${target.displayName} остановился`)
 }
+
 function goto(message, target) {
     const x = Number(message.split(" ")[1])
     const y = Number(message.split(" ")[2])
@@ -48,22 +52,32 @@ function goto(message, target) {
         console.log(clc.blue('Иду в кординаты', x, y, z))
     }
 }
+
 function health(target) {
     bot.chat(`/msg ${target} здоровье:${bot.health} насыщение:${bot.food} позиция ${bot.entity.position}`)
-    console.log(clc.red(`  здоровье:${bot.health} `), clc.yellow(`насыщение:${bot.food} `), clc.blue(`позиция  x: ${bot.entity.position.x} y: ${bot.entity.position.y} z: ${bot.entity.position.z} `))
+    console.log(
+        clc.red(`  здоровье:${bot.health} `),
+        clc.yellow(`насыщение:${bot.food} `),
+        clc.blue(`позиция  x: ${bot.entity.position.x} y: ${bot.entity.position.y} z: ${bot.entity.position.z} `),
+    )
 }
+
 function examination(username, message) {
 
     const defaultMove = new Movements(bot)
     const target = bot.players[username] ? bot.players[username].entity : null
-    switch (message.split(" ")[0]){
-        case "goto": goto(message, target)
+    switch (message.split(" ")[0]) {
+        case "goto":
+            goto(message, target)
             break;
-        case "come": come(username)
+        case "come":
+            come(username)
             break;
-        case "stop": stop(username)
+        case "stop":
+            stop(username)
             break;
-        case "health": health()
+        case "health":
+            health()
             break;
     }
 }
@@ -76,12 +90,13 @@ app.post("/", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     if (request.body.mes !== undefined) {
         if (request.body.name === bot.username) {
-            if (request.body.mes.split('')[0] === '/') {examination(request.body.name, request.body.mes.split('').slice(1).join(''))}
-            else {bot.chat(request.body.mes)}
-
+            if (request.body.mes.split('')[0] === '/') {
+                examination(request.body.name, request.body.mes.split('').slice(1).join(''))
+            } else {
+                bot.chat(request.body.mes)
+            }
         }
     }
-
 });
 bot.on('chat', async (username, message) => {
     console.log(`<${username}> ${message}`)
