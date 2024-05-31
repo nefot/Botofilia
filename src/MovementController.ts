d5bd4ef67a73a40310018f12cdb65bfc76288d47
artyom <artyom27000@mail.ru>
2024-05-31 17:16:31 +0300

    ghjtyjhytj

--- src/MovementController.ts
+++ src/MovementController.ts
@@ -3,52 +3,100 @@ import { ChatController } from './ChatController';
 import { pathfinder, Movements, goals } from 'mineflayer-pathfinder';
 import { Terminal } from './terminal';
 const { GoalNear, GoalFollow } = goals;
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+
+
+
+
 
 class MovementController {
     private bot: mineflayer.Bot;
     private chat: ChatController;
     private t: Terminal;
-
+    private deathPoint: { x: number, y: number, z: number } | null = null;
+    private logDir: string = path.join(__dirname, 'Death');
+    private logFilePath: string = path.join(this.logDir, 'Death.log');
+    private lastDamageSource: string | null = null;
     constructor(bot: mineflayer.Bot, chat: ChatController, terminal: Terminal) {
         this.bot = bot;
         this.chat = chat;
         this.t = terminal;
         this.bot.loadPlugin(pathfinder);
         this.setupEventHandlers();
+        this.ensureLogDirectory();
     }
 
     private setupEventHandlers(): void {
         this.bot.on('goal_reached', () => {
-            this.t.printMessage('Goal reached');
-            // Add any actions needed when the goal is reached
+            this.t.printMessage('Цель достигнута');
+        });
+
+
+        this.bot.on('death', () => {
+            if (this.bot.entity.position) {
+                this.deathPoint = {
+                    x: this.bot.entity.position.x,
+                    y: this.bot.entity.position.y,
+                    z: this.bot.entity.position.z
+                };
+                const deathMessage = `Точка смерти сохранена: x=${this.deathPoint.x}, y=${this.deathPoint.y}, z=${this.deathPoint.z}`;
+                console.log(deathMessage);
+                this.t.printMessage(deathMessage);
+
+                // Добавление данных о смерти в лог-файл
+                const dimension = this.bot.game.dimension;
+                const date = new Date().toISOString();
+
+                const logMessage = `[${date}] {${this.bot.entity.username}} Dimension: ${dimension}, Coordinates: x=${this.deathPoint.x}, y=${this.deathPoint.y}, z=${this.deathPoint.z}\n`;
+
+                fs.appendFile(this.logFilePath, logMessage, (err) => {
+                    if (err) {
+                        console.error('Ошибка при записи в лог-файл', err);
+                    } else {
+                        console.log('Информация о смерти сохранена в лог-файл');
+                    }
+                });
+            }
         });
     }
 
-    public gotoBlock(x: string, y: string, z: string, username: string): string {
-        const _x: number = Number(x);
-        const _y: number = Number(y);
-        const _z: number = Number(z);
+    private ensureLogDirectory(): void {
+        if (!fs.existsSync(this.logDir)) {
+            fs.mkdirSync(this.logDir, { recursive: true });
+        }
+    }
+    public gotoBlock(x: string, y: string, z: string, username: string, ...other: string[]):string | undefined {
+        const _x: number = x == "~"? this.bot.entity.position.x: Number(x);
+        const _y: number = y == "~"? this.bot.entity.position.y: Number(y)
+        const _z: number = z == "~"? this.bot.entity.position.z: Number(z)
         const _target = this.bot.players[username] ? this.bot.players[username].entity : null;
 
         if (isNaN(_x) || isNaN(_y) || isNaN(_z)) {
-            const errorMessage = `Invalid coordinates: ${x} ${y} ${z}`;
+            const errorMessage = `Неверные координаты: ${x} ${y} ${z}`;
             this.chat.sendDirectMessage(username, errorMessage);
             console.error(errorMessage);
             return errorMessage;
         }
 
         this.bot.pathfinder.setGoal(new GoalNear(_x, _y, _z, 0));
-        const defaultMove = new Movements(this.bot);
+        const defaultMove:Movements = new Movements(this.bot);
         const path = this.bot.pathfinder.getPathTo(defaultMove, new GoalNear(_x, _y, _z, 0));
 
-        this.t.printMessage(`Bot is going to coordinates:, ${_x}, ${_y}, ${_z}`);
-        this.chat.sendDirectMessage(username, `Going to coordinates: ${_x} ${_y} ${_z}`);
-        return `Going to coordinates: ${_x} ${_y} ${_z}`;
+        this.t.printMessage(`Бот идет в координаты: ${_x}, ${_y}, ${_z}`);
+        this.chat.sendDirectMessage(username, `Бот идет в координаты: ${_x} ${_y} ${_z}`);
+        this.bot.once('goal_reached', ():void => {
+            this.t.printMessage(`Pathfinder достиг цели по координатам: ${_x}, ${_y}, ${_z}`);
+            this.chat.sendDirectMessage(username, `Достигнутые координаты: ${_x} ${_y} ${_z}`);
+        });
+
     }
 
     public stopBot(username: string): string {
         this.bot.pathfinder.setGoal(null);
-        const message = 'Stopping';
+        const message = 'Остановка';
         this.chat.sendDirectMessage(username, message);
         this.t.printMessage(message);
         return message;
@@ -59,12 +107,12 @@ class MovementController {
 
         if (_target) {
             this.bot.pathfinder.setGoal(new GoalFollow(_target, 1), true);
-            const message = 'Coming to player';
+            const message:string = 'Следование за игроком';
             this.chat.sendDirectMessage(username, message);
             this.t.printMessage(message);
             return message;
         } else {
-            const errorMessage = `Unable to find entity for player: ${username}`;
+            const errorMessage:string = `Не удалось найти объект для игрока: ${username}`;
             this.chat.sendDirectMessage(username, errorMessage);
             console.error(errorMessage);
             return errorMessage;
